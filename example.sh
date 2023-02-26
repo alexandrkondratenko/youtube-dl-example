4c6c91f80afc0016ad2358e9a37e07f0ed2e1b2c
Alexandr Kondratenko <alexandr.kondratenko83@gmail.com>
2023-02-26 20:39:16 +0200

    Add initial example.

---
+++ example.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# ffmpeg required to merge audio and video streams by youtube-dl itself.
+brew install ffmpeg
+
+# Latest youtube-dl.
+git clone https://github.com/ytdl-org/youtube-dl.git
+
+# EXAMPLE:
+# https://www.youtube.com/watch?v=f0yJbrXmtOU
+# youtube id = "f0yJbrXmtOU"
+
+id="f0yJbrXmtOU"
+
+# Retrieve formats.
+# Not required, for demonstration purposes only.
+python3 youtube-dl/youtube_dl/__main__.py -F "$id"
+
+# Actually download and merge streams.
+# This is the main command line to use.
+python3 youtube-dl/youtube_dl/__main__.py -f bestvideo+bestaudio -o "downloads/%(id)s.%(ext)s" "$id"
